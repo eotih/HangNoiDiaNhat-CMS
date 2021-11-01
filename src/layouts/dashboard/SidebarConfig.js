adb019f29434eab1532bcac382c0a73fa7d3b03e
My Nguyen <myoutybay@gmail.com>
2021-11-01 16:27:29 +0700

    sidebar

--- src/layouts/dashboard/SidebarConfig.js
+++ src/layouts/dashboard/SidebarConfig.js
@@ -6,6 +6,9 @@ import fileTextFill from '@iconify/icons-eva/file-text-fill';
 import lockFill from '@iconify/icons-eva/lock-fill';
 import personAddFill from '@iconify/icons-eva/person-add-fill';
 import alertTriangleFill from '@iconify/icons-eva/alert-triangle-fill';
+import keypadoutline from '@iconify/icons-eva/keypad-outline';
+import briefcasefill from '@iconify/icons-eva/briefcase-fill';
+import carfill from '@iconify/icons-eva/car-fill';
 
 // ----------------------------------------------------------------------
 
@@ -13,24 +16,139 @@ const getIcon = (name) => <Icon icon={name} width={22} height={22} />;
 
 const sidebarConfig = [
   {
+    Header: 'Quản lý',
+    path: false
+  },
+  {
     title: 'dashboard',
     path: '/dashboard/app',
     icon: getIcon(pieChart2Fill)
   },
   {
-    title: 'user',
+    title: 'Component  ',
     path: '/dashboard/user',
-    icon: getIcon(peopleFill)
+    icon: getIcon(briefcasefill),
+    children: [
+      {
+        title: 'State',
+        path: '/dashboard/state'
+      },
+      {
+        title: 'Services',
+        path: '/dashboard/services'
+      },
+      {
+        title: 'Roles',
+        path: '/dashboard/Roles'
+      },
+      {
+        title: 'Utilities',
+        path: '/dashboard/utilities'
+      },
+      {
+        title: 'Category',
+        path: '/dashboard/category'
+      },
+      {
+        title: 'Field',
+        path: '/dashboard/field'
+      },
+      {
+        title: 'Payment',
+        path: '/dashboard/payment'
+      },
+      {
+        title: 'Brands',
+        path: '/dashboard/brands'
+      }
+    ]
   },
   {
-    title: 'product',
+    title: 'Product',
     path: '/dashboard/products',
-    icon: getIcon(shoppingBagFill)
+    icon: getIcon(shoppingBagFill),
+    children: [
+      {
+        title: 'Quản Lý Product',
+        path: '/dashboard/manage-products'
+      },
+      {
+        title: 'Hình Ảnh Product',
+        path: '/dashboard/Album-products'
+      }
+    ]
   },
   {
-    title: 'blog',
+    title: 'Organization',
     path: '/dashboard/blog',
-    icon: getIcon(fileTextFill)
+    icon: getIcon(peopleFill),
+    children: [
+      {
+        title: 'Organization',
+        path: '/dashboard/organization'
+      },
+      {
+        title: 'Bank',
+        path: '/dashboard/bank'
+      },
+      {
+        title: 'Account',
+        path: '/dashboard/account'
+      }
+    ]
+  },
+  {
+    title: 'Delivery',
+    path: '/dashboard/delivery',
+    icon: getIcon(carfill),
+    children: [
+      {
+        title: 'Shipper',
+        path: '/dashboard/shipper'
+      },
+      {
+        title: 'Shipping Department',
+        path: '/dashboard/shipping-department'
+      },
+      {
+        title: 'Tracking order',
+        path: '/dashboard/Tracking-order'
+      },
+      {
+        title: 'Identity Card',
+        path: '/dashboard/indentity-card'
+      }
+    ]
+  },
+  {
+    title: 'Management',
+    path: '/dashboard/management',
+    icon: getIcon(keypadoutline),
+    children: [
+      {
+        title: 'Order',
+        path: '/dashboard/order'
+      },
+      {
+        title: 'OrderDetails',
+        path: '/dashboard/order-details'
+      }
+    ]
+  },
+  {
+    title: 'Actical',
+    path: '/dashboard/actical',
+    icon: getIcon(fileTextFill),
+    children: [
+      {
+        title: 'Post',
+        path: '/dashboard/post'
+      },
+      {
+        title: 'Field',
+        path: '/dashboard/field'
+      }
+    ]
   },
   {
     title: 'login',
